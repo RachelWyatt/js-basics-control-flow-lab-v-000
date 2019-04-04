e7eebc8101cb19532d4f26c5db09c7e3d34c243b
Rachel Wyatt <rw126@evansville.edu>
2019-04-04 15:13:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let result
     result = "This one is on me!"
   } else if (someValue >= 2000) {
     result = "I will gladly take your thirty bucks."
-  } else if (someValue >2500)
+  } else if (someValue >2500) {
+    result = "No can do."
+  }
   return result
 }
