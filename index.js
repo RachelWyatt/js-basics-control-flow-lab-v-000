8c1c8eb44a81e419c56bb6f7d813a8dc96658860
Rachel Wyatt <rw126@evansville.edu>
2019-04-04 15:11:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code in this file!
-let scuberGreetingForFeet(someValue)
+function scuberGreetingForFeet(someValue)
+let result
   if (someValue <= 400){
     "This one is on me!"
   } else if (someValue >= 3000) {
