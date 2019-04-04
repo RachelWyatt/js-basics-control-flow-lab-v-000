71c6a6927e62b78f6c14cfbfcfcff6b634646df2
Rachel Wyatt <rw126@evansville.edu>
2019-04-04 15:21:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function ternaryCheckCity(value) {
   return result
 }
 
-function ternaryCheckCity(value) {
+function switchOnCharmFromTip(value) {
   let result;
   switch (value) {
     case value = "NYC":
